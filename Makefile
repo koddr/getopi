44d8fc07ef725baaaaff7d672b411f7b5d7a87b9
Vic Shóstak <vikkyshostak@gmail.com>
2020-01-27 22:47:02 +0300

    Add reset.css

--- Makefile
+++ Makefile
@@ -1,16 +1,13 @@
 .PHONY: front-start
 
 front-start:
-	cd ./frontend \
-	&& npm start
-	@echo "[✅] Preact app is running!"
+	@cd ./frontend && npm start
+	@echo "[OK] Preact app is running!"
 
 front-lint:
-	cd ./frontend \
-	&& npm run lint
-	@echo "[✅] Preact app was linted!"
+	@cd ./frontend && npm run lint
+	@echo "[OK] Preact app was linted!"
 
 front-build-prod:
-	cd ./frontend \
-	&& npm run build
-	@echo "[✅] Preact app was builded!"
+	@cd ./frontend && npm run build
+	@echo "[OK] Preact app was builded!"
--- frontend/src/components/ui/footer/index.js
+++ frontend/src/components/ui/footer/index.js
@@ -1,19 +1,19 @@
 // Style
-import style from './style';
+import style from "./style";
 
 // Router link
-import { Link } from 'preact-router/match';
+import { Link } from "preact-router/match";
 
 const Footer = () => (
-	<header class={style.footer}>
-		<div class={style.copyright}>
-      &copy; 2019. <a href="https://1wa.co">True web artisans</a>
-		</div>
-		<nav class={style.menu}>
-			<Link href="/login">Login</Link>
-			<Link href="/register">Register</Link>
-		</nav>
-	</header>
+  <footer class={style.footer}>
+    <div class={style.copyright}>
+      &copy; 2020. <a href="https://1wa.co">True web artisans</a>
+    </div>
+    <nav class={style.menu}>
+      <Link href="/login">Login</Link>
+      <Link href="/register">Register</Link>
+    </nav>
+  </footer>
 );
 
 export default Footer;
