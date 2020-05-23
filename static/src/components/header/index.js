bfb8c424789cf307539a950541f7a89c3ad56ce0
Vic Shóstak <vikkyshostak@gmail.com>
2020-05-23 13:02:44 +0300

    Add Header component

--- static/src/components/header/index.js
+++ static/src/components/header/index.js
@@ -7,13 +7,25 @@ import style from "./style";
 const Header = () => (
   <header class={style.header}>
     <div class={style.logo}>
-      <Link activeClassName={style.active} href="/">
-        <img src="/assets/images/getopi_short-logo.svg" alt="GetOpi Logo" />
+      <Link href="/">
+        <img src="/assets/images/getopi_short-logo.svg" alt="getopi logo" />
       </Link>
     </div>
     <nav>
-      <Link activeClassName={style.active} href="/">
+      <Link href="/">
         <img src="/assets/icons/search.svg" alt="search icon" />
+      </Link>
+      <Link href="/">
+        <img src="/assets/icons/plus-circle.svg" alt="plus circle icon" />
+      </Link>
+      <Link href="/">
+        <img src="/assets/icons/bell.svg" alt="bell icon" />
+      </Link>
+      <Link href="/">
+        <img src="/assets/icons/settings.svg" alt="settings icon" />
+      </Link>
+      <Link href="/">
+        <img src="/assets/icons/no-avatar.svg" alt="no avatar icon" />
       </Link>
     </nav>
   </header>
